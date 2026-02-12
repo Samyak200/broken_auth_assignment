2b5db4ee46a757479f96260097b758cf185c9dac
Samyak Jain <samyakjain1908@gmail.com>
2026-02-12 23:36:45 +0530

    Assignment completed with output.txt

---
+++ utils/tokenGenerator.js
@@ -0,0 +1,17 @@
+const crypto = require("crypto");
+const { getSecretFromDB } = require("./mockDB");
+
+const generateToken = async (email) => {
+  try {
+    const secret = await getSecretFromDB();
+
+    return crypto
+      .createHmac("sha256", secret)
+      .update(email)
+      .digest("base64");
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { generateToken };
